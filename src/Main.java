a5a4fe83f86092fbb0093d75d6b9e67c29ad6214
rafandco <rafandpufff@gmail.com>
2025-10-30 14:26:30 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;        // ObjectOutputStream / ObjectInputStream
+import java.nio.file.*;  // Paths y Files
+import java.util.*;      // List y ArrayList
+
+public class Main {
+
+    // Fichero binario donde guardar/leer la lista serializada
+    private static final Path FICHERO = Paths.get("personas.dat");
+
+    public static void main(String[] args) {
+        // Realiza las siguientes tareas, agrupa en funciones cuando veas oportuno
+        //TODO: 1) Crear una lista de personas
+
+        //TODO: 2) Añadir personas (por consola)
+
+        //TODO: 3) Serializar la lista al fichero
+
+        //TODO: 4) Deserializar la lista desde el fichero
+
+        //TODO: 5) Imprimir por consola las personas deserializadas
+
+    }
+}
